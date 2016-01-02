3d01e09f4f8a506a15c4dd9c6044b9089f3aae86
alex <sania_free@i.ua>
2016-01-02 19:52:04 +0200

    add basket and catalog

--- js/common.js
+++ js/common.js
@@ -13,6 +13,29 @@ $(document).ready(function() {
 	  $( "ul.nav" ).slideToggle( "slow");
 	});
 
+	//sidebar menu
+	$( ".sidebar_menu_block" ).hide();
+
+	$( ".js-show-sid-menu" ).click(function() {
+	  $(this).next().slideToggle( "slow");
+	});
+
+	//plusminus
+	$('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+    
 	//bxslider
 	$('.bxslider').bxSlider({
 	  nextSelector: '#slider-next',
